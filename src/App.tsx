0ba562662235d4e8456aad339b06455d90a180c1
chapter <chuongninolxag@gmail.com>
2025-07-19 21:32:06 +0700

    feat: add Pricing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SpinnerFullPage from "./components/spinner/SpinnerFullPage";
-import PageNotFound from "./pages/not-found/PageNotFound";
 import Homepage from "./pages/home-page/Homepage";
+import PageNotFound from "./pages/not-found/PageNotFound";
 import Product from "./pages/product/Product";
+import Pricing from "./pages/pricing/Pricing";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           <Routes>
             <Route index element={<Homepage />} />
             <Route path="product" element={<Product />} />
-            <Route path="pricing" element={<>Pricing</>} />
+            <Route path="pricing" element={<Pricing/>} />
             <Route path="login" element={<>Login</>} />
             <Route path="*" element={<PageNotFound />} />
           </Routes>
